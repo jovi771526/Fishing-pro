12a7ee79775f9864f587628c9581d7818400438c
jovi771526 <cd9c7wgfnc@privaterelay.appleid.com>
2026-04-24 23:39:20 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const cacheName = "rima-pro-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        "/Fishing-pro/",
+        "/Fishing-pro/index.html"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
